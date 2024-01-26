02d68079953715294112aaa59caa5543ce465a84
chee <yay@chee.party>
2024-01-26 13:19:40 +0000

    add command line interface

---
+++ libraries/from-bodyxml/cli.js
@@ -0,0 +1,17 @@
+// todo use package name
+import convert from "./index.js"
+
+import {createReadStream} from "fs"
+let path = process.argv[2]
+
+let stream = path && path != "-" ? createReadStream(path) : process.stdin
+
+// goodbye memory
+let xml = await new Promise((yay, boo) => {
+	let string = ""
+	stream.on("data", data => (string += data.toString("utf8")))
+	stream.on("end", () => yay(string))
+	stream.on("error", boo)
+})
+
+process.stdout.write(JSON.stringify(convert(xml), null, "\t"))
