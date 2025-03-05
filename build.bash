a9161f0efb400a8947cfe3d3e07f876a5851c926
chee <yay@chee.party>
2025-03-05 11:17:17 +0000

    Make the json schemas strict
    disallow additional properties, and make required fields required

--- build.bash
+++ build.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 node tools/maketypes <README.md> content-tree.ts
 tsc -d content-tree.ts
-typescript-json-schema content-tree.ts ContentTree.full.Root > schemas/content-tree.schema.json
-typescript-json-schema content-tree.ts ContentTree.transit.Root > schemas/transit-tree.schema.json
-typescript-json-schema content-tree.ts ContentTree.transit.Body > schemas/body-tree.schema.json
+typescript-json-schema --noExtraProps --required content-tree.ts ContentTree.full.Root > schemas/content-tree.schema.json
+typescript-json-schema --noExtraProps --required content-tree.ts ContentTree.transit.Root > schemas/transit-tree.schema.json
+typescript-json-schema --noExtraProps --required content-tree.ts ContentTree.transit.Body > schemas/body-tree.schema.json
 rm content-tree.ts
 rm content-tree.js
