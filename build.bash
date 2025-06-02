81dac65bb5c6c54213dae19f7ad1d9d4e2d8db26
Arjun Gadhia <arjungadhia@gmail.com>
2025-06-02 13:58:38 +0100

    WIP: demo what experiments in content tree could look like

--- build.bash
+++ build.bash
@@ -4,5 +4,7 @@ tsc -d content-tree.ts
 typescript-json-schema --noExtraProps --required content-tree.ts ContentTree.full.Root > schemas/content-tree.schema.json
 typescript-json-schema --noExtraProps --required content-tree.ts ContentTree.transit.Root > schemas/transit-tree.schema.json
 typescript-json-schema --noExtraProps --required content-tree.ts ContentTree.transit.Body > schemas/body-tree.schema.json
+typescript-json-schema --noExtraProps --required content-tree.ts ContentTree.transit.ExperimentalBodyBlock > schemas/experiments.schema.json
+
 rm content-tree.ts
 rm content-tree.js
